5cc48e7915b97a4e262030ec2e8ac0b0422b9bdc
syougo_takayama <shougo_takayama@commude.co.jp>
2021-12-06 15:11:59 +0900

    add null-and-undefined.ts

---
+++ src/null-and-undefined.ts
@@ -0,0 +1,16 @@
+export {};
+
+//null型
+
+//nullのみであることを明示的に指定する
+let absence: null = null;
+//以下はエラーになる
+// absence = "hello";
+
+
+//undefined型
+
+//undefinedのみであることを明示的に指定する
+let data:undefined = undefined
+//以下はエラーになる
+// data = 123;
--- src/void.ts
+++ src/void.ts
@@ -4,6 +4,11 @@ export {};
 //関数にreturnを含まない、データを返さない場合はanyまたはvoidを指定する必要がある。
 //void : 明示的に何も返さないことを表す(期待する)型。
 
+//void使用例
+function returnNothing(): void {
+  console.log("I don't return anything!");
+}
+
 //もしくは return undefind;を関数内に記載する。
 //このようにするとエラーは出なくなるが、関数の本来の目的とことなるため、使用してはならない。
 
@@ -11,9 +16,5 @@ export {};
 //   return undefined;
 // }
 
-//void使用例
-function returnNothing(): void {
-  console.log("I don't return anything!");
-}
 
 console.log(returnNothing());
