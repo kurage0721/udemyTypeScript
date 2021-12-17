c71bb0da917fe6915cb41048bb2e8fefbb951d88
syougo_takayama <shougo_takayama@commude.co.jp>
2021-12-17 09:18:38 +0900

    名前空間の学習

---
+++ src/namespaces.ts
@@ -0,0 +1,35 @@
+export {};
+
+//名前空間による同名クラスの宣言
+namespace English {
+  //クラスにexportをつけないとnew演算子によるインスタンス宣言などのアクセスができない
+  export class Person {
+    constructor(
+      public firstName: string,
+      public middleName: string,
+      public lastName: string
+    ) {}
+  }
+}
+const michael = new English.Person('Michael', 'Joseph', 'Jackson');
+console.log(michael);
+
+//名前空間内の入れ子
+namespace Japanese {
+  //入れ子の名前空間にはexportをつけることによりJapanese.Tokyo.Personのようなアクセスが可能になる。
+  export namespace Tokyo {
+    export class Person {
+      constructor(public name: string) {}
+    }
+  }
+  export namespace Osaka {
+    export class Person {
+      constructor(public name: string) {}
+    }
+  }
+}
+
+const me = new Japanese.Tokyo.Person('はむさん');
+const wai = new Japanese.Osaka.Person('はむさんやねん');
+console.log(me.name);
+console.log(wai.name);
