0415423ddbb7dab85712b28f79cf3749c027ee20
ShogoTakayama <seamoonjellyfish@gmail.com>
2022-01-04 01:23:32 +0900

    mapped type

--- src/mapped-types.ts
+++ src/mapped-types.ts
@@ -8,6 +8,7 @@ type Profile = {
 type PartialType = Partial<Profile>;
 type PropertyTypes = keyof Profile;
 
+//in keyof でprofileのkeyに設定された型を一つずつ取り出して独自の方として定義している。
 type Optional<T> = {
   [P in keyof T]?: T[P] | null;
 };
